4dac1cf1a5a4cec7d4f966f91a2f03fe7139a1c9
Zayed Bin Gazi <zayedbingazidev@gmail.com>
2021-07-23 14:47:07 +0400

    Share Code on Git 6

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import data from './data';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">Amazona</a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {
+            data.products.map((product) => {
+              return (
+                <div key={product._id} className="card">
+                  <a href={`/product/${product._id}`}>
+                    <img className="medium" src={product.image} href={`/product/${product._id}`} alt={product.name} />
+                  </a>
+                  <div className="card-body">
+                    <a href={`/product/${product._id}`}>
+                      <h2>{product.name}</h2>
+                    </a>
+                    <div className="rating">
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                    </div>
+                    <div className="price">AED {product.price}</div>
+                  </div>
+                </div>    
+              )
+            })
+          }
+        </div>
+      </main>
+      <footer className="row center">
+            All Rights Reserved
+      </footer>
+  </div>
+  );
+}
+
+export default App;
