d675b0c3c94ae8e6b8c82be88d5891e399d0ca1b
Zayed Bin Gazi <zayedbingazidev@gmail.com>
2021-07-29 23:04:27 +0400

    Added alert css, created LoadingBox and MessageBox componet with the necessary changes in the HomeScreen and ProductScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Product from '../components/Product';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
-      const [products, setProducts] = useState([])
+      const [products, setProducts] = useState([]);
+      const [loading, setloading] = useState(false);
+      const [error, seterror] = useState(false)
       useEffect(() => {
             const fethcData = async () => {
-                  const { data } = await axios.get('/api/products');
-                  setProducts(data);
+                  try {
+                        setloading(true);
+                        const { data } = await axios.get('/api/products');
+                        setloading(false);
+                        setProducts(data);
+                  } catch (err) {
+                        seterror(err.message);
+                        setloading(false);
+                  }
             }
             fethcData()
       }, []) 
       {/* [] will run this function only once */}
       return (
             <div>
-                  <div className="row center">
-                        {
-                              products.map((product) => {
-                              return (
-                              <Product key={product._id} product={product}/>
-                              )
-                              })
-                        }
-                  </div>
+                  {loading? (<LoadingBox/>):error?(<MessageBox variant="danger">{error}</MessageBox>):(
+                              <div className="row center">
+                              {
+                                    products.map((product) => {
+                                          return (
+                                                      <Product key={product._id} product={product}/>
+                                                )
+                                          }
+                                    )
+                              }
+                        </div>
+                        )
+                  }
+                  
             </div>
       )
 }
